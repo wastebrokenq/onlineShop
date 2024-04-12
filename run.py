bc7e2c2062a0c8b48130f6b510d1d73f9fa5a43e
waste <belkharoevibragimq@gmail.com>
2024-04-12 16:09:17 +0300

    added automatic addition of products from the list and product-container

--- run.py
+++ run.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    products = [
+        {'name': 'Кокоа (мягкая игрушка)', 'price': 1500, 'image': 'cocoa.jpg'},
+        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
+        {'name': 'Плюшевый слон', 'price': 1800, 'image': 'cocoa.jpg'}
+    ]
+    return render_template("index.html", products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
