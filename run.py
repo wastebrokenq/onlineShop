25b596944a65cb46d7cf2bbdd5a41719ca669177
waste <belkharoevibragimq@gmail.com>
2024-04-12 17:41:52 +0300

    update css styles

--- run.py
+++ run.py
@@ -20,9 +20,5 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/contacts")
-def contacts():
-    return render_template("contacts.html")
-
 if __name__ == '__main__':
     app.run(debug=True)
