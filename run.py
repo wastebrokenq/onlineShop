a6ac75f5158d8bad45012c3bad32b672066330d1
waste <belkharoevibragimq@gmail.com>
2024-04-12 16:22:51 +0300

    a fixed basement has been added

--- run.py
+++ run.py
@@ -9,9 +9,20 @@ def index():
     products = [
         {'name': 'Кокоа (мягкая игрушка)', 'price': 1500, 'image': 'cocoa.jpg'},
         {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
+        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
+        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
+        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
         {'name': 'Плюшевый слон', 'price': 1800, 'image': 'cocoa.jpg'}
     ]
     return render_template("index.html", products=products)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
