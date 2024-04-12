c09c6ed1db151387d8b22798fdb00b5a444ba8c5
waste <belkharoevibragimq@gmail.com>
2024-04-13 01:43:28 +0300

    added card.html and styles for this page

--- run.py
+++ run.py
@@ -2,23 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+products = [
+    {'name': 'Кокоа (мягкая игрушка)', 'price': 1500, 'images': 'cocoa.jpg'},
+    {'name': 'Игрушечный мишка', 'price': 1200, 'images': 'cocoa.jpg'},
+    {'name': 'Игрушечный мишка', 'price': 1200, 'images': 'cocoa.jpg'},
+    {'name': 'Игрушечный мишка', 'price': 1200, 'images': 'cocoa.jpg'},
+    {'name': 'Игрушечный мишка', 'price': 1200, 'images': 'cocoa.jpg'},
+    {'name': 'Плюшевый слон', 'price': 1800, 'images': 'cocoa.jpg'}
+]
 
 @app.route("/")
 @app.route("/index")
 def index():
-    products = [
-        {'name': 'Кокоа (мягкая игрушка)', 'price': 1500, 'image': 'cocoa.jpg'},
-        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
-        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
-        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
-        {'name': 'Игрушечный мишка', 'price': 1200, 'image': 'cocoa.jpg'},
-        {'name': 'Плюшевый слон', 'price': 1800, 'image': 'cocoa.jpg'}
-    ]
     return render_template("index.html", products=products)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/product/<int:index>")
+def product(index):
+    product = products[index]
+    return render_template("card.html", product=product)
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
