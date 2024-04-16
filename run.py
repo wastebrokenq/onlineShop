02325031bbeda522c835e0f940cd22cab0492fba
waste <belkharoevibragimq@gmail.com>
2024-04-17 01:37:42 +0300

    added admin account and admin db table with encryptor

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SECRET_KEY'] = 'lorsikponosik'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onlineShop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,11 +33,23 @@ class Product(db.Model):
     def image_list(self):
         return self.images.split(',') if self.images else []
 
-with app.app_context():
-    db.create_all()
 
-admin = Admin(app, name='ProductAdmin', template_mode='bootstrap3')
-admin.add_view(ModelView(Product, db.session))
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+
+admin = Admin(app, name='ProductAdmin', template_mode='bootstrap3', index_view=MyAdminIndexView())
+admin.add_view(MyModelView(Product, db.session))
 
 @app.route("/")
 @app.route("/index")
@@ -41,5 +66,33 @@ def product(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template("card.html", product=product)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('admin.index'))
+        return 'Invalid username or password'
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        if User.query.filter_by(username='admin').first() is None:
+            hashed_password = generate_password_hash('d1letantoss676', method='pbkdf2:sha256')
+            admin_user = User(username='admin', password=hashed_password, is_admin=True)
+            db.session.add(admin_user)
+            db.session.commit()
+
     app.run(debug=True)
